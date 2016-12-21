b09ff1a0c955d5f60ae81d88ae0cbd4a39c0a516
vinimaks <vinimaks56@mail.ru>
2016-12-21 03:22:34 +0200

    Подключенный движок

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * Created by Vinimaks
+ */
+require_once 'core/model.php';
+require_once 'core/view.php';
+require_once 'core/controller.php';
+require_once 'core/route.php';
+Route::start();
--- views/login_view.php
+++ views/login_view.php
@@ -3,12 +3,12 @@
 <head>
 	<title>Вход</title>
 	<meta charset="utf-8">
-	<script type="text/javascript" src="js/mainjs.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+        <script type="text/javascript" src="../template/js/mainjs.js"></script>
+        <link rel="stylesheet" type="text/css" href="../template/css/main.css">
 </head>
 <body>
 <div class="wrapper">
-	<form id="login_title">
+	<form id="login_title" method="POST" action="">
 		<h2>Вход</h2>
 		<div class="login_parag_left">
 			<label for="input_1" class="login_parag_left_float">Email</label>
@@ -23,12 +23,14 @@
 			<input type="password" name="pwd" tabindex="2" id="input_2" required="required">
 		</div>
 		<div id="capture_text">Пожалуйста, введите число, которое Вы видите на картинке</div>
-		<div id="capture_img"><img src="capture.jpg"></div>
+                <div id="capture_img"><img src="template/img/capture.jpg"></div>
 		<div id="capture_input"><input type="text" name="capture" tabindex="3"></div>
 		<button type="submit" tabindex="4">Войти</button>
 		<a href="#">Восстановление пароля</a>
 		<a href="#">Не пришло сообщение со ссылкой для активации аккаунта</a>
 	</form>
 </div>
+<?php extract($data); ?>
+<p style="color:green"> <?php echo $pwd ?> </p>
 </body>
 </html>
