f71d490adb863cadab302d8a4f7ffd8710d92d02
Bafyn <nikitin2705@mail.ru>
2016-12-21 07:44:10 +0200

    header + footer

---
+++ views/footer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+	<link href="../template/css/main.css" rel="stylesheet" type="text/css">
+	<script src="mainjs.js"></script>
+</head>
+<body>
+
+<div class="footer_container">
+    <section class="section">
+        <div class="section_white_right_skew"></div>
+    </section>
+
+    <footer class="footer">
+        <div class="section_inner">
+            <p class="site_name">Крутой сайт</p>
+          	<div class="footer_info clearfix">
+            	<div class="copyright">
+              		<p class="color_gray">Все права защищены © 2016</p>
+              		<p>Cлужба поддержки:&nbsp;
+              			<a href="mailto:support@moemnenie.ru" class="gl_link color_blue">support@site.ru</a>
+              		</p>
+            	</div>
+            	<div class="footer_contacts" style="font-size: 96%">
+               		<p>
+			     		<a href="http://www.moemnenie.ru/ru/policy" class="gl_link color_blue">Политика конфиденциальности</a> |
+			     		<a href="http://www.moemnenie.ru/ru/user_agreement" class="gl_link color_blue">Пользовательское соглашение</a> |
+				 		<a href="http://www.moemnenie.ru/ru/jobs" class="gl_link color_red">Вакансии</a> |
+				 		<a href="http://www.moemnenie.ru/ru/mts-winners" class="gl_link color_red">Победители конкурсов</a>
+			   		</p>
+				</div> 			
+          	</div>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
---
+++ views/header.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Header</title>
+	<link href="../template/css/main.css" rel="stylesheet" type="text/css">
+	<script src="mainjs.js"></script>
+</head>
+<body>
+<div class="header_container">
+	<header class="header">
+		<nav class="nav_menu clearfix">
+			<a href="" class="logo">
+				<img src="../template/img/logo.png" width="200px" height="50px" alt="Крутой сайт">
+			</a>
+
+			<div></div>
+
+			<ul class="main_menu">
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">О проекте</span>
+					</a>
+				</li>
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">Пункт меню</span>
+					</a>
+				</li>
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">Пункт меню</span>
+					</a>
+				</li>
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">Помощь</span>
+					</a>
+				</li>
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">Регистрация</span>
+					</a>
+				</li>
+				<li class="main_menu_item">
+					<a href="" class="main_menu_item_link">
+						<span class="main_menu_item_text">Вход</span>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+
+	<div class="main_slider inner_mod">
+        <ul class="header_line clearfix">
+        	<li class="header_l_item header_l_item_blue"></li>
+            <li class="header_l_item header_l_item_blue_2"></li>
+            <li class="header_l_item header_l_item_green"></li>
+            <li class="header_l_item header_l_item_blue_3"></li>
+            <li class="header_l_item header_l_item_olive"></li>
+            <li class="header_l_item header_l_item_blue"></li>
+        </ul>
+        <div class="slider_wrapper">
+            <div class="slide">
+                <div>
+                	<span class="site_info_header">Какой-то крутой сайт</span>
+                </div>
+                <img src="../template/img/main_header.jpg" class="slide_img max_w_mod mob_only">
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
